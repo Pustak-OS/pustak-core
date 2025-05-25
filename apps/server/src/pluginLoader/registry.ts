2e1116600e9f9b4aaee2dd92e291a29c4ce32799
Ayush Pahwa <ayushpahwa96@gmail.com>
2025-05-25 15:16:57 +0700

    feat(plugin): implement PluginRegistry and define plugin types
    This commit introduces the PluginRegistry class for managing plugins, allowing for the addition, retrieval, and status checking of plugins. It also defines essential types for plugin configuration and context, enhancing the plugin system's structure and usability.

---
+++ apps/server/src/pluginLoader/registry.ts
@@ -0,0 +1,28 @@
+import { InternalPlugin } from "./types";
+
+export class PluginRegistry {
+  private plugins: Map<string, InternalPlugin>;
+
+  constructor() {
+    this.plugins = new Map();
+  }
+
+  addPlugin(name: string, plugin: InternalPlugin): void {
+    if (this.plugins.has(name)) {
+      throw new Error(`Plugin with name ${name} is already registered`);
+    }
+    this.plugins.set(name, plugin);
+  }
+
+  getPlugin(name: string): InternalPlugin | undefined {
+    return this.plugins.get(name);
+  }
+
+  isPluginLoaded(name: string): boolean {
+    return this.plugins.has(name);
+  }
+
+  getAllPlugins(): InternalPlugin[] {
+    return Array.from(this.plugins.values());
+  }
+}
---
+++ apps/server/src/pluginLoader/types.ts
@@ -0,0 +1,25 @@
+import Router from "@koa/router";
+import Koa from "koa";
+
+// Plugin developer interface
+export interface PluginConfig {
+  name: string;
+  serverEntry: string;
+}
+
+export interface PluginContext {
+  app: Koa;
+  router: Router;
+}
+
+export interface PluginHooks {
+  firstTimeSetup?: (context: PluginContext) => Promise<void>;
+  registerRoute?: (context: PluginContext) => Promise<void>;
+}
+
+// Internal types
+export interface InternalPlugin {
+  config: PluginConfig;
+  hooks: PluginHooks;
+  isInitialized: boolean;
+}
